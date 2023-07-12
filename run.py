549b6525e1ab33f9bbefadee3db6345034cc60a3
KnightJC <>
2023-07-12 20:47:44 +0000

    Adding invalid input as well as guessed words

--- run.py
+++ run.py
@@ -7,3 +7,25 @@ Choices of words for hangman
 def pick_word():
     word_list = ["hangman", "python", "programming", "computer", "game", "player"]
     return random.choice(word_list)
+
+
+def display_word(word, guessed_letters):
+    displayed_word = ""
+    for letter in word:
+        if letter in guessed_letters:
+            displayed_word += letter + " "
+        else:
+            displayed_word += "_ "
+    return displayed_word.strip()
+
+    """
+    Adding letters to be guessed as well as an invalid input if entered incorrectly
+    """
+
+def get_guess():
+    while True:
+        guess = input("Guess a letter: ").lower()
+        if len(guess) == 1 and guess.isalpha():
+            return guess
+        else:
+            print("Invalid input. Please enter a single letter.") 
