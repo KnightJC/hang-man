8045175e070b68fa753972915e2529d53e8f834b
KnightJC <>
2023-07-12 22:14:21 +0000

    added end loop to exit game or play again

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import random
 Choices of words for hangman
 """
 
+
 def pick_word():
     word_list = ["hangman", "python", "programming", "computer", "game", "player"]
     return random.choice(word_list)
@@ -17,10 +18,13 @@ def display_word(word, guessed_letters):
         else:
             displayed_word += "_ "
     return displayed_word.strip()
+    
 
     """
-    Adding letters to be guessed as well as an invalid input if entered incorrectly
+    Adding letters to be guessed as well as an invalid
+    input if entered incorrectly
     """
+
 
 def get_guess():
     while True:
@@ -34,31 +38,32 @@ def get_guess():
     Adding ASCII art for better visual experience as well as attempts made
     """
 
+
 def draw_hangman(attempts):
     stages = [
         '''
             --------
             |      |
-            |      O
-            |     \|/
-            |      |
-            |     / \
+            |
+            |
+            |
+            |
             -
         ''',
         '''
             --------
             |      |
             |      O
-            |     \|/
-            |      |
-            |     /
+            |
+            |
+            |
             -
         ''',
         '''
             --------
             |      |
             |      O
-            |     \|/
+            |      |
             |      |
             |
             -
@@ -76,7 +81,7 @@ def draw_hangman(attempts):
             --------
             |      |
             |      O
-            |      |
+            |     \|/
             |      |
             |
             -
@@ -85,62 +90,79 @@ def draw_hangman(attempts):
             --------
             |      |
             |      O
-            |
-            |
-            |
+            |     \|/
+            |      |
+            |     /
             -
         ''',
         '''
-            --------
+           --------
             |      |
-            |
-            |
-            |
-            |
+            |      O
+            |     \|/
+            |      |
+            |     / \
             -
         '''
     ]
-    return stages[6 - attempts] 
+    return stages[6 - attempts]
+
 
 """
 adding words picked, correct, incorrect and attemps
 """
 
+
 def play_game():
-    word = pick_word()
-    guessed_letters = []
-    incorrect_guesses = []
-    attempts = 6
-
-    print("Welcome to Hangman!")
-    print("The word has", len(word), "letters.")
-
     while True:
-        print(draw_hangman(attempts))
-        print("Attempts left:", attempts)
-        print("Word:", display_word(word, guessed_letters))
-        print("Incorrect guesses:", " ".join(incorrect_guesses))
+        word = pick_word()
+        guessed_letters = []
+        incorrect_guesses = []
+        attempts = 6
 
-        if "_" not in display_word(word, guessed_letters):
-            print("Congratulations! You guessed the word correctly!")
+        print("Welcome to Hangman!")
+        print("The word has", len(word), "letters.")
+
+        while True:
+            print(draw_hangman(attempts))
+            print("Attempts left:", attempts)
+            print("Word:", display_word(word, guessed_letters))
+            print("Incorrect guesses:", " ".join(incorrect_guesses))
+
+            if "_" not in display_word(word, guessed_letters):
+                print("Congratulations! You guessed the word correctly!")
+                break
+
+            guess = get_guess()
+
+            if guess in guessed_letters or guess in incorrect_guesses:
+                print("You already guessed that letter. Try again.")
+                continue
+
+            if guess in word:
+                guessed_letters.append(guess)
+                print("Correct guess!")
+            else:
+                incorrect_guesses.append(guess)
+                attempts -= 1
+                print("Incorrect guess.")
+
+                if attempts == 0:
+                    print("Sorry, you ran out of attempts. The word was:", word)
+                    break
+
+        """
+        Added end game loop to play again or exit game
+        """
+
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        
+        while play_again != "yes" and play_again != "no":
+            play_again = input("Invalid input. Please enter 'yes' or 'no': ")
+        
+        if play_again == "no":
             break
 
-        guess = get_guess()
-
-        if guess in guessed_letters or guess in incorrect_guesses:
-            print("You already guessed that letter. Try again.")
-            continue
-
-        if guess in word:
-            guessed_letters.append(guess)
-            print("Correct guess!")
-        else:
-            incorrect_guesses.append(guess)
-            attempts -= 1
-            print("Incorrect guess.")
-
-            if attempts == 0:
-                print("Sorry, you ran out of attempts. The word was:", word)
-                break
+    print("Thanks for playing!")
 
 play_game()
