1fcd5525f45c6e93d442e3c4f8df7318bee009f4
KnightJC <>
2023-07-12 20:32:04 +0000

    Added hangman stages to game

--- run.py
+++ run.py
@@ -1,3 +1,57 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+"""
+Hangman stages
+"""
+hangman_stages = [
+'''
+                                    +---+
+                                    |   |
+                                    O   |
+                                   /|\  |
+                                   / \  |
+                                        |
+                                    =========''', '''
+                                    +---+
+                                    |   |
+                                    O   |
+                                   /|\  |
+                                   /    |
+                                        |
+                                    =========''', '''
+                                    +---+
+                                    |   |
+                                    O   |
+                                   /|\  |
+                                        |
+                                        |
+                                    =========''', '''
+                                    +---+
+                                    |   |
+                                    O   |
+                                   /|   |
+                                        |
+                                        |
+                                    =========''', '''
+                                    +---+
+                                    |   |
+                                    O   |
+                                    |   |
+                                        |
+                                        |
+                                    =========''', '''
+                                    +---+
+                                    |   |
+                                    O   |
+                                        |
+                                        |
+                                        |
+                                    =========''', '''
+                                    +---+
+                                    |   |
+                                        |
+                                        |
+                                        |
+                                        |
+                                    ========='''
+]
